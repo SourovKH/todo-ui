49b442c5ac55ab1cbb66b96c49718e878af877de
Sourov <SourovKH@users.noreply.github.com>
2024-02-19 23:14:09 +0530

    Add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import LoginPage from "./components/Login";
 
 const Homepage = () => <p>Home Page</p>;
-const LoginPage = () => <p>Login Page</p>;
 const ErrorPage = () => <p>Error Page</p>;
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="todo-app">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
       <Routes>
         <Route path="/login" element={<LoginPage />}/>
         <Route path="/" element={<Homepage />} />
---
+++ src/components/Login.jsx
@@ -0,0 +1,90 @@
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useNavigate } from 'react-router-dom'
+
+const defaultTheme = createTheme();
+
+export default function LoginPage() {
+  const navigate = useNavigate();
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      userId: data.get("userId"),
+      password: data.get("password"),
+    });
+    navigate("/");
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="user-id"
+              label="User Id"
+              name="userId"
+              autoComplete="userId"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <Link href="/todo/signup" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </div>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
