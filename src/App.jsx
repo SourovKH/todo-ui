cf9e78e0f80f030dacfebac5a8ad79d5f6afe314
Sourov <SourovKH@users.noreply.github.com>
2024-02-19 21:40:01 +0530

    Add Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Homepage = () => <p>Home Page</p>;
+const LoginPage = () => <p>Login Page</p>;
+const ErrorPage = () => <p>Error Page</p>;
+
 function App() {
   return (
-    <div>This is Todo App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />}/>
+        <Route path="/" element={<Homepage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
