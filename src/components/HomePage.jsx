3b167b2604aae136940b3610d337bb9ab8a9009d
Sourov Halder <sourovhalder@Sourovs-MacBook-Pro.local>
2024-03-03 18:28:39 +0530

    Create home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable react/prop-types */
+import {Link} from "react-router-dom";
+import {DashBoardButton, HomePageImage, TemplateBox, TemplateBoxHeading} from "../styles/home-page.styles.js";
+
 const HomePage = () => {
   return (
-    <div style={{
-      margin: '0px'
-    }}>
-      Home Page
-    </div>
+    <div style={{padding: '1em', display: 'flex', alignItems: "center", justifyContent: 'center', height: '90%'}}>
+      <TemplateBox>
+        <TemplateBoxHeading>Welcome to TODO</TemplateBoxHeading>
+        <div style={{color: "white"}}>Track you work on daily basis</div>
+        <DashBoardButton>
+          <Link to="/todo-dashboard" style={{textDecoration: 'none', color: 'black'}}>Get Started</Link>
+        </DashBoardButton>
+      </TemplateBox>
+      <HomePageImage>
+        <img src="../../public/images/home-page-image.jpeg" style={{height: '90%', aspectRatio: '1'}}></img>
+      </HomePageImage>
+  </div>
   )
 }
 
---
+++ src/styles/home-page.styles.js
@@ -0,0 +1,31 @@
+import {styled} from "@mui/material";
+
+export const TemplateBox = styled('div')({
+  width: '40%',
+  height: '15em',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  boxShadow: "0 3px 10px rgb(0 0 0 / 0.6)",
+  backgroundColor: 'cornflowerblue',
+});
+
+export const TemplateBoxHeading = styled('div')({
+  fontSize: "2em",
+  fontWeight: "600",
+  color: 'white'
+});
+
+export const DashBoardButton = styled('div')({
+  textAlign: 'center',
+  width: '50%',
+  padding: '0.5em',
+  borderRadius: '50px',
+  backgroundColor: 'white',
+});
+
+export const HomePageImage = styled('div')({
+  height: '100%',
+  aspectRatio: "1"
+})
